4f07d9bcfa7babb0696d9dc915a06b1db523b9e2
itzortzis <i.n.tzortzis@gmail.com>
2024-05-15 14:09:30 +0300

    Update: version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='bad',
-    version='0.1.1',
+    version='0.1.2',
     author='I.N.Tzortzis',
     author_email='i.n.tzortzis@gmail.com',
     description='BAD_tool installation',
